871f2eb7000acd5b8295d1780ca55f8a85744fe7
pareeksp47 <pareeksp47@gmail.com>
2021-06-24 00:45:36 +0530

    Update ContactusController.java

--- src/main/java/com/exchange/isep/controller/ContactusController.java
+++ src/main/java/com/exchange/isep/controller/ContactusController.java
@@ -5,7 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-
+/**
+ */
 @Controller
 public class ContactusController {
 	
